62be13564bca6f6056bc52bd296853abfbcaf27a
Gerrit Wanderer <gerrit.wanderer@gmail.com>
2025-08-28 22:11:38 +0200

    tweak neotree

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+
+local opt = vim.opt
+opt.clipboard = "unnamedplus"
+opt.relativenumber = false
+
+-- LazyVim root dir detection
+-- Each entry can be:
+-- * the name of a detector function like `lsp` or `cwd`
+-- * a pattern or array of patterns like `.git` or `lua`.
+-- * a function with signature `function(buf) -> string|string[]`
+vim.g.root_spec = { { ".git" }, "cwd", "lsp" }
+
+vim.g.snacks_animate = false
